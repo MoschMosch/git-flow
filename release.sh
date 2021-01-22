26b3eafeb4e8f0af8081fcb69352954af3551114
MoschMosch <moitz@online.de>
2021-01-22 09:49:20 +0100

    refine release script

--- release.sh
+++ release.sh
@@ -6,20 +6,58 @@ devBranch=develop
 masterBranch=master
 releaseBranch="release-tmp"
 pathToPom="pom.xml"
+remoteName="origin"
 
 # Read level from input
 versionLevel=$1
 
 # Validate input
-if [ -z $versionLevel ]; then 
+if [ -z $versionLevel ]; then
     # Set a default
     versionLevel="patch"
 else
     if [ $versionLevel != "minor" ] && [ $versionLevel != "major" ] && [ $versionLevel != "patch" ]; then
-        echo "Invalid input: Please use 'patch' or 'minor' or 'major'!\nNo input defaults to 'patch'."
+        echo "#########Invalid input: Please use 'patch' or 'minor' or 'major'!\n#########No input defaults to 'patch'."
         exit 1
     fi
 fi
+
+# Check or local changes
+LocalStatus=$(git status --short)
+if [ -z "$LocalStatus" ] ; then
+    echo "######### No local changes! Proceeding.."
+else
+    echo "#########You have local changes.\n#########Please commit or revert them before you do a release!\n#########Use 'git status' to see them."
+fi
+
+# Check or local commits
+git fetch
+echo "######### Fetched remote state"
+LocalCommits=$(git cherry $remoteName/$devBranch $devBranch)
+if [ -z "$LocalCommits" ] ; then
+    echo "######### No local commits! Proceeding.."
+else
+    echo "#########You have local commits which are not on the remote yet.\n#########Please push them or reset your branch back to the remote state!\n#########Use 'git cherry $remoteName/$devBranch $devBranch' to see the commits."
+    echo "#########Use 'git push' to push and 'git reset --hard $remoteName/$devBranch' to revert back to remote state."
+fi
+
+# Handle remote commits
+RemoteCommits=$(git cherry $devBranch $remoteName/$devBranch)
+if [ -z "$RemoteCommits" ] ; then
+    echo "######### No remote commits! Proceeding.."
+else
+    echo "#########There are new commits on the remote branch. Going fast-forward..."
+    git merge --ff-only
+fi
+
+# Check for SNAPSHOT statements
+SnapshotDependency=$(cat pom.xml| grep SNAPSHOT)
+if [ -z "$SnapshotDependency" ] ; then
+    echo "######### No SNAPSHOT referrences found! Proceeding.."
+else
+    echo "#########Your pom.xml does include the word SNAPSHOT. Please remove it."
+fi
+
 echo "######### Going to increase ${versionLevel}Version!"
 
 # Create the release branch from the develop branch
@@ -71,7 +109,7 @@ git branch -d $releaseBranch
 echo "######### Deleted release branch"
 
 # Publish changes
-git push origin master
-git push origin develop
+git push $remoteName $masterBranch
+git push $remoteName $devBranch
 echo "######### Published changes"
 echo "######### Finished successful"
